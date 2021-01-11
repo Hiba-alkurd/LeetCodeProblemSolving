7d7bcdb28a28e3d7bbca0165706aa8b6c20beca5
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-11 11:08:21 +0200

    remove duplicates using currentIndex to keep track of the current pos and currentDup to keep track of the duplicate value

---
+++ RemoveDuplicates/main.cs
@@ -0,0 +1,32 @@
+﻿//this is the solution on Leetcode
+public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+        
+        if(nums.Length == 0) return 0;
+        int currentIndex = 1;
+        int currentDup = nums[0];
+        int size = nums.Length;
+            
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == currentDup)
+            {
+                size--;
+            }
+            else if (nums[i] == nums[i-1])
+            {
+                currentDup = nums[i];
+                size--;
+            }
+            else
+            {
+                nums[currentIndex] = nums[i];
+                currentIndex++;
+            }
+                
+        }
+        
+        return size;
+    }
+}
