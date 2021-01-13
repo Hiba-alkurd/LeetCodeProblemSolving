d8a75066f07afd9dde12cdfb849e71524ce21d42
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-13 09:14:16 +0200

    solve longest-common-prefix

--- Longest Common Prefix/main.cs
+++ Longest Common Prefix/main.cs
@@ -1,35 +1,34 @@
-﻿using System;
-
-namespace Problems
+﻿namespace DefaultNamespace
 {
-    class Program
+    public class main
     {
         public string LongestCommonPrefix(string[] strs)
         {
+            if(strs.Length == 0) return "";
+        
             string common = strs[0];
             bool found = false;
+        
             foreach (var word in strs)
             {
                 var subString = word;
                 found = false;
+                
                 if (common == "") return common;
                 
                 while (!found && common!="")
                 {
-                    Console.WriteLine($"common: {common} sub: {subString}");
+                    
                     if (common == subString)
                     {
-                        Console.WriteLine("equals");
                         found = true;
                     }
                     else if (common.Length > subString.Length)
                     {
-                        Console.WriteLine("common");
                         common = common.Remove(subString.Length);
                     }
                     else if (common.Length < subString.Length)
                     {
-                        Console.WriteLine("substring");
                         subString = subString.Remove(common.Length);
                     }
                     else
@@ -38,20 +37,13 @@ namespace Problems
                         subString = subString.Remove(subString.Length - 1);
                     }
 
-                    found = common == subString;
+                    found = (common == subString);
 
                 }
 
             }
 
             return common;
-        }
-        static void Main(string[] args)
-        {
-            var tem = new Program();
-            string[] strs = {"dog","racecar","car"};
-            Console.WriteLine(tem.LongestCommonPrefix(strs));
-            //Console.WriteLine(strs[0].Remove(strs[0].Length-1));
         }
     }
 }
