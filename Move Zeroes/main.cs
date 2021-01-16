1639f3f944fdb50bff8f0689cd4caa57e349a1cd
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-16 12:44:00 +0200

    move zeroes

--- Move Zeroes/main.cs
+++ Move Zeroes/main.cs
@@ -1,22 +1,12 @@
-﻿using System;
-
-namespace Problems
+﻿namespace DefaultNamespace
 {
-    class Program
+    public class main
     {
         public void MoveZeroes(int[] nums)
         {
-
-            int Zerocount = 0;
-            int moveIndex = 0;
-            bool moved = true;
             int tem = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i]!=0)
-                {
-                    continue;
-                }
                 if (nums[i] == 0)
                 {
                     for (int j = i; j < nums.Length; j++)
@@ -32,16 +22,6 @@ namespace Problems
                 }
                 
             }
-        }
-        static void Main(string[] args)
-        {
-            int[] num = new[] {0,1,0,3,12};
-            new Program().MoveZeroes(num);
-            foreach (var VARIABLE in num)
-            {
-                Console.Write(VARIABLE+" ");
-            } 
-            
         }
     }
 }
