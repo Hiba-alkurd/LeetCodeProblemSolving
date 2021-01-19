5f5b2731a8df84a9c6f5990f5e48ec02a2b5e03f
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-19 13:24:41 +0200

    solve two-sum Problem

---
+++ Two Sum/main.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace
+{
+    public class main
+    {
+        public int[] TwoSum(int[] nums, int target) {
+            var secondNum = 0;
+            var size = nums.Length;
+            for(int i = 0; i < size ; i++){
+                secondNum = target-nums[i];
+                int index = Array.LastIndexOf(nums, secondNum);
+                if(index >= 0 && i != index){
+                    return new int[]{i, index};
+                }
+            }
+
+            return null;
+        }
+    }
+}
