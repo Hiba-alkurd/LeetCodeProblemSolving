d4196152e612199c5f09254fe4f4f15a9378bb90
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-23 17:34:53 +0200

    delete unnecessary files

--- Valid Parentheses/main.cs
+++ Valid Parentheses/main.cs
@@ -9,23 +9,15 @@
             char expectedParenthesis = '-';
             foreach (var par in s)
             {
-                if (par == '[' || par == '{' || par =='(')
-                {
-                    openingPar.Push(par);
-                }
-                else if (openingPar.Count != 0 && par == expectedParenthesis)
-                {
-                    openingPar.Pop();
-                }
-                else
-                {
-                    return false;
-                }
+                if (par == '[' || par == '{' || par =='(') openingPar.Push(par);
+                else if (openingPar.Count != 0 && par == expectedParenthesis) openingPar.Pop();
+                else return false;
+                
                 if(openingPar.Count != 0) expectedParenthesis = closingPar(openingPar.Peek());
             }
-            
             return openingPar.Count == 0;
         }
+        
         public char closingPar(char par)
         {
             if (par == '[') return ']';
