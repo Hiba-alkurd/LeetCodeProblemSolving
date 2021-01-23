d212e51d0153d8c2fd0813a17910bec503f9e2fa
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-23 17:47:27 +0200

    delete unnecessary files

--- Two Sum/main.cs
+++ Two Sum/main.cs
@@ -3,16 +3,13 @@
     public class main
     {
         public int[] TwoSum(int[] nums, int target) {
-            var secondNum = 0;
+            var secondNum, index = 0;
             var size = nums.Length;
             for(int i = 0; i < size ; i++){
                 secondNum = target-nums[i];
-                int index = Array.LastIndexOf(nums, secondNum);
-                if(index >= 0 && i != index){
-                    return new int[]{i, index};
-                }
+                index = Array.LastIndexOf(nums, secondNum);
+                if(index >= 0 && i != index) return new int[]{i, index};
             }
-
             return null;
         }
     }
