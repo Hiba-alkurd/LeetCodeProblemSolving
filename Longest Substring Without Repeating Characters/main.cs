eef4ea28fb68a62d11e8bafd6b5358c2bcef5c07
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-23 17:44:40 +0200

    delete unnecessary files

--- Longest Substring Without Repeating Characters/main.cs
+++ Longest Substring Without Repeating Characters/main.cs
@@ -3,9 +3,12 @@
     public class main
     {
         public int LengthOfLongestSubstring(String s) {
-            int n = s.Length, ans = 0;
+            
+            int n = s.Length, length = 0;
+            
             var map = new Dictionary<char, int>(); 
             for (int j = 0, i = 0; j < n; j++) {
+                
                 if (map.ContainsKey(s[j])) {
                     i = Math.Max(map[s[j]], i);
                     map[s[j]] = j + 1;
@@ -14,10 +17,10 @@
                 {
                     map.Add(s[j], j + 1);
                 }
-                ans = Math.Max(ans, j - i + 1);
-                    
+                
+                length = Math.Max(length, j - i + 1);
             }
-            return ans;
+            return length;
         }
     }
 }
