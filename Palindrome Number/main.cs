c0204649833bded2d39217034d23d5fea9cb8069
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-23 17:28:52 +0200

    delete unnecessary files

--- Palindrome Number/main.cs
+++ Palindrome Number/main.cs
@@ -4,18 +4,14 @@
     {
         public bool IsPalindrome(int x)
         {
-            int y = 0;
-            
             if (x < 0 || (x!=0 && x % 10==0)) return false;
             
+            int y = 0;
             while (x > y)
             {
-                Console.WriteLine($"x= {x} y= {y}");
                 y = y * 10 + x % 10;
                 x /= 10;
-                Console.WriteLine($"x= {x} y= {y}");
             }
-
             return (x == y || x == y/10);
         }
     }
