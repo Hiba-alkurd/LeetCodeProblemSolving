c94957d7c13ee532462448665bc7136485214b72
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-23 17:49:38 +0200

    delete unnecessary files

--- Jump Game/main.cs
+++ Jump Game/main.cs
@@ -9,14 +9,12 @@
            for (int i = 0; i < nums.Length-1; )
            {
                if (nums[i] == 0) return false;
+               
                var maxIndex = 1;
                for (var j = 1; j <= nums[i]; j++)
                {
                    if (j + i >= nums.Length - 1) return true;
-                   if (nums[j + i] - (nums[i] - j) > (nums[maxIndex+i] - (nums[i] - maxIndex)))
-                   {
-                       maxIndex = j;
-                   }
+                   if (nums[j + i] - (nums[i] - j) > (nums[maxIndex+i] - (nums[i] - maxIndex))) maxIndex = j;
                }
                i = maxIndex + i;
            }
