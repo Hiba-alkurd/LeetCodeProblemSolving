1fde85580655c21a01bdb78e7dcd047cf66a9802
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-27 09:36:44 +0200

    better performance, memory wise

--- Maximum Depth of Binary Tree/main.cs
+++ Maximum Depth of Binary Tree/main.cs
@@ -3,6 +3,10 @@
     public int MaxDepth(TreeNode root) {
         
         if(root == null) return 0;
+        if(root.right == null && root.left == null) return 1;
+        if(root.right == null) return MaxDepth(root.left) + 1;
+        if(root.left == null) return MaxDepth(root.right) + 1;
+        
         
         return Math.Max(MaxDepth(root.right) + 1, MaxDepth(root.left) + 1);
         
