065afa4b6eae468ef34cd654279d9e25d960a7bc
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-27 08:27:22 +0200

    solve max depth of binary tree using recursion

---
+++ Maximum Depth of Binary Tree/main.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    
+    public int MaxDepth(TreeNode root) {
+        
+        if(root == null) return 0;
+        
+        return Math.Max(MaxDepth(root.right) + 1, MaxDepth(root.left) + 1);
+        
+    }
+    
+}
